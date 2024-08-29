773be230d2d55c4241f79201234b9f6b5b05af49
Lukas Turcani <lukasturcani93@gmail.com>
2024-08-29 12:16:02 +0100

    Add todos

--- explore-bo-implementations/BayBE.py
+++ explore-bo-implementations/BayBE.py
@@ -21,6 +21,7 @@ from baybe.targets import NumericalTarget, TargetMode
 
 
 def rerun_bo(
+    # TODO: what is the type here?
     campaign_db_entry,
     new_measurements: pd.DataFrame,
     batch_size: int = 1,
@@ -31,6 +32,7 @@ def rerun_bo(
 
 
 def run_bo(
+    # TODO: what is the type here?
     experiment_info,
     target: str,
     opt_type: TargetMode = TargetMode.MAX,
@@ -39,16 +41,21 @@ def run_bo(
     variable_type_dict = pd.read_json(experiment_info.variables)
     baybe_paramter_list = []
     for col in variable_type_dict.columns:
+        # TODO: type of .T says it returns a Series, but variable is called a DataFrame
         df = variable_type_dict[col].T
         if df["parameter-type"] == "subs":
             baybe_paramter_list.append(
                 SubstanceParameter(
-                    f"{col}", data=df["json"], encoding=f"{df['encoding']}"
+                    f"{col}",
+                    data=df["json"],
+                    # TODO: does df['encoding'] need to be cast to a string?
+                    encoding=f"{df['encoding']}",
                 )
             )
         elif df["parameter-type"] == "cat":
             baybe_paramter_list.append(
                 CategoricalParameter(
+                    # TODO: does df['encoding'] need to be cast to a string?
                     f"{col}",
                     values=pd.read_json(df["json"])[f"{col}"],
                     encoding="OHE",
@@ -57,6 +64,7 @@ def run_bo(
         elif df["parameter-type"] == "int":
             baybe_paramter_list.append(
                 NumericalDiscreteParameter(
+                    # TODO: does df['encoding'] need to be cast to a string?
                     f"{col}",
                     values=list(
                         np.arange(int(df["min"]), int(df["max"]), 1.0)
@@ -66,7 +74,9 @@ def run_bo(
         else:
             baybe_paramter_list.append(
                 NumericalContinuousParameter(
-                    f"{col}", bounds=(float(df["min"]), float(df["max"]))
+                    # TODO: does df['encoding'] need to be cast to a string?
+                    f"{col}",
+                    bounds=(float(df["min"]), float(df["max"])),
                 )
             )
     search_space = SearchSpace.from_product(baybe_paramter_list)
@@ -80,6 +90,7 @@ def run_bo(
         initial_recommender=RandomRecommender(),
         recommender=SequentialGreedyRecommender(
             surrogate_model=GaussianProcessSurrogate(),
+            # TODO: does this need to be cast to a string?
             acquisition_function_cls=f"{experiment_info.acqFunc}",
             allow_repeated_recommendations=False,
             allow_recommending_already_measured=False,
